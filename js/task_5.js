97e6189432d7000842b824d0366579f53bddfc61
Daryna <66668459+DarynaProkopchuk@users.noreply.github.com>
2020-09-28 05:00:42 +0400

    task 5done

---
+++ js/task_5.js
@@ -0,0 +1,15 @@
+
+function checkForSpam (message) { 
+    
+   let string = message.toLowerCase();
+   return string.includes('sale') || string.includes('spam');}
+  
+  console.log(checkForSpam('Latest technology news')); // false
+  
+  console.log(checkForSpam('JavaScript weekly newsletter')); // false
+  
+  console.log(checkForSpam('Get best sale offers now!')); // true
+  
+  console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+
