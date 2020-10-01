c6f6d4a6db3a3611c7ba4ebf25fca0d2d65ded4d
Daryna <66668459+DarynaProkopchuk@users.noreply.github.com>
2020-10-02 02:41:01 +0400

    fuuh

--- js/task_6.js
+++ js/task_6.js
@@ -2,7 +2,7 @@ function mapArray(array) {
     'use strict';
     const numbers = new Array(array.length);
     for(let i = 0; i < array.length; i += 1) {
-      // Write code under this line
+      numbers[i] = array[i] * 10;
       
     }
     return numbers;
---
+++ js/task_9.js
@@ -0,0 +1,33 @@
+function isLoginValid (login, min = 4, max = 16) {
+    // Write code under this line
+     const valid = login.length >= 4 && login.length <= 16;
+     return valid;
+  }
+  
+  function isLoginUnique  (allLogins, login) {
+    'use strict';
+    // Write code under this line
+      const loginUnique = allLogins.includes(login);
+      if (loginUnique === true) {
+        return false;
+      } 
+        return true;
+  }
+  
+  function addLogin (allLogins, login) {
+    'use strict';
+    const SUCCESS = 'Логин успешно добавлен!';
+    const REFUSAL = 'Такой логин уже используется!';
+    const ERROR = 'Ошибка! Логин должен быть размером от 4 до 16 символов';
+    let message;
+    // Write code under this line    
+      if (isLoginValid(login) === false) {
+      return (message = ERROR);
+    } else if (isLoginUnique(allLogins, login) === false) {
+      return (message = REFUSAL);
+    }
+      allLogins.push(login);
+      return (message = SUCCESS);
+  }
+  
+  const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
